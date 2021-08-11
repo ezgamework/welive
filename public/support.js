40ca0096649c0677fc80f15584c986294f8e12e2
ez <shalinglom@gmail.com>
2021-08-11 17:00:17 +0800

    反转译换行标签\n

--- public/support.js
+++ public/support.js
@@ -485,7 +485,7 @@ function showNext(hide) {
 
 //格式化输出信息
 function format_output(data) {
-	data.replace(/\\/g, "<br/>");
+	data = data.replace(/\\/g, "<br/>");
 
 	//生成URL链接
 	data = data.replace(/((((https?|ftp):\/\/)|www\.)([\w\-]+\.)+[\w\.\/=\?%\-&~\':+!#;]*)/ig, function($1){return getURL($1);});
